177b1df9f2eacec14080a6aa5328bb78cb580131
ladj rachid <rladj@live.fr>
2018-07-17 15:49:55 +0200

    Get color ajoutée

--- src/main/fr/formation/puissance4/Piece/Jeton.java
+++ src/main/fr/formation/puissance4/Piece/Jeton.java
@@ -1,16 +1,38 @@
 package fr.formation.puissance4.Piece;
 
+
+
 import javafx.scene.paint.Color;
+
 import javafx.scene.shape.Circle;
 
 
+
+
+
 public class Jeton {
+
     Circle  circle;
 
+
+
     public Jeton(Circle circle) {
+
         this.circle = circle;
+
     }
-     public void setColor(Color color){
+
+    public void setColor(Color color){
+
         this.circle.setFill(color);
-     }
+
+    }
+
+    public Color getColor(){
+
+        return (Color) circle.getFill();
+
+    }
+
 }
+
