ac9960275134db5fffb6426fc1c3a7c0a8bb03d3
ladj rachid <rladj@live.fr>
2018-07-18 16:36:59 +0200

    partie plein et deux exception

---
+++ src/main/fr/formation/puissance4/Exception/IntputMismatchException.java
@@ -0,0 +1,7 @@
+package fr.formation.puissance4.Exception;
+
+public class IntputMismatchException extends Exception{
+    public IntputMismatchException(String message) {
+        super(message);
+    }
+}
--- src/main/fr/formation/puissance4/Joueur/JoueurHumain.java
+++ src/main/fr/formation/puissance4/Joueur/JoueurHumain.java
@@ -1,9 +1,12 @@
 package fr.formation.puissance4.Joueur;
 
 import fr.formation.puissance4.Board.Board;
+import fr.formation.puissance4.Exception.IntputMismatchException;
 import fr.formation.puissance4.Exception.LigneChoisiException1;
 import javafx.scene.paint.Color;
 
+import java.util.InputMismatchException;
+import java.util.MissingFormatArgumentException;
 import java.util.Scanner;
 
 public class JoueurHumain extends Joueur {
@@ -12,12 +15,18 @@ public class JoueurHumain extends Joueur {
     }
 
 
-    public int choisirColonne() {
-        System.out.println(" Entrez un nombre entre 1 et 6:");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt()-1;
+    public int choisirColonne()throws IntputMismatchException {
 
-    }
+
+            System.out.println(" Entrez un nombre entre 1 et 6:");
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt()- 1;
+
+            if(input < 0 || input >6)
+                throw new InputMismatchException(" Le nombre est invalide");
+
+            return input;
+        }
 
     public int choisirLigne(int colonne) throws LigneChoisiException1 {
         int ligne;
@@ -32,20 +41,18 @@ public class JoueurHumain extends Joueur {
         return ligne;
     }
 
+    public boolean estPlein() {
+        int grille[][] = new int[6][7];
 
-    /*  public boolean estPlein() {
-        int grille[][]= new int [6][7];
-
-        for(int colonne=0; colonne<6; colonne++){
-            for(int ligne=0; ligne<7; ligne++){
-             if(grille[colonne][ligne]){
-                 return false;
-             }
+        for (int colonne = 0; colonne < 6; colonne++) {
+            for (int ligne = 0; ligne < 7; ligne++) {
+                if (board.getJetons()[ligne][colonne].getColor().equals(Color.TRANSPARENT)) {
+                    return false;
+                }
             }
-
         }
         return true;
-    }*/
+    }
 
 
     public String choisirColor() {
@@ -58,7 +65,14 @@ public class JoueurHumain extends Joueur {
     @Override
     public String envoyer() {
 
-        int colonne = choisirColonne();
+        int colonne = 0;
+        try {
+            colonne = choisirColonne();
+        } catch (IntputMismatchException e) {
+            e.printStackTrace();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(" Index est trop grand ");
+        }
         int ligne = 0;
         try {
             ligne = choisirLigne(colonne);
