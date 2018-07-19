db107b60623dcbf7b966e25f34ad24c6bc95da1d
unknown <david.parto@hotmail.com>
2018-07-19 12:08:05 +0200

    verify le gagnant methode vertical et horizontal

--- src/main/fr/formation/puissance4/Joueur/Joueur.java
+++ src/main/fr/formation/puissance4/Joueur/Joueur.java
@@ -54,11 +54,11 @@ public abstract class Joueur {
 
     protected boolean checkVeritcal (int x, int y){
         boolean result = false;
-        int begin = x <=3 ? 0 : y-3;
+        int begin = x <=3 ? 0 : x-3;
         int end = x <=3 ? x+3 : 5;
         int check = begin;
 
-        while( !result && check < end ){
+        while( !result && end- check <= 3 ){
 
             for(int i=begin;i<check+4;i++){
                 result &= myMoves[i][y];
@@ -73,6 +73,57 @@ public abstract class Joueur {
     }
 
     protected boolean checkDiagonal(int x, int y){
+        boolean result = false;
+
+        if( (x<= 2) && (y>=2) )return false;
+        if( (x<= 2) && (y>=2) )return false;
+
+        int beginX =0;
+        int endX = 0;
+        int beginY = 0;
+
+        if(x+y <6){
+          endX = x+y;
+          beginY = x+y ;
+
+        }else{
+            beginY = y;
+
+        }
+
+        while( !result &&   endX - beginX <= 3){
+
+            for(int i=beginX; i<beginX+4;i++){
+                result &= myMoves[i][beginY -i];
+            }
+            beginX++;
+            beginY--;
+        }
+       /*
+
+        int endY =0;
+        int endX =0;
+
+
+
+
+        if(x+y <6){
+          beginX = x+y;
+          endY = x+y;
+          endX =0;
+        }else{
+
+        }
+
+        int checkX = beginX;
+        int checkY = beginY;
+*/
+
+
+        return result;
+    }
+
+ protected boolean checkDiagonalReverse(int x, int y){
         return false;
     }
 
